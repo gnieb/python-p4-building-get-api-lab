d25d154437005cba1ad9efe0b08476325749e883
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-05 12:25:27 -0400

    models and routes completed

--- server/app.py
+++ server/app.py
@@ -20,19 +20,65 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+          200,
+          {"Content-Type": "application/json"}
+    )
+
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id==id).first()
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    
+    for bg in BakedGood.query.order_by(BakedGood.price).all():
+        bg_dict = bg.to_dict()
+        baked_goods.append(bg_dict)
+
+    response = make_response(
+        baked_goods,
+          200,
+          {"Content-Type": "application/json"}
+    )
+
+    return response
+    
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensy = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    expensy_dict = most_expensy.to_dict()
+    response = make_response(
+        expensy_dict,
+        200,
+        {"Content-Type":"application/json"}
+    )
+
+    return response
+
 
 if __name__ == '__main__':
-    app.run(port=555, debug=True)
+    app.run(port=5555, debug=True)
